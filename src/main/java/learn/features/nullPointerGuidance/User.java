f9a0d3406fc6210b7855d0b17c18aa214f25f338
jeevakirtan <jeevakirtan.p@thoughtworks.com>
2023-04-16 12:57:40 +0530

    Refactored NullPointerGuidance and added tests

---
+++ src/main/java/learn/features/nullPointerGuidance/User.java
@@ -0,0 +1,27 @@
+package learn.features.nullPointerGuidance;
+
+public class User {
+    public UserDetail userDetail;
+    String id;
+    String email;
+
+    public static void main(String[] args) {
+        User john = new User();
+        john.userDetail.address.firstLine = "ABC Apartment";
+    }
+
+    public static class UserDetail {
+        public Address address;
+        String name;
+        String mobileNumber;
+    }
+
+    public static class Address {
+        public String firstLine;
+        String secondLine;
+        String city;
+        String pincode;
+    }
+}
+
+
---
+++ src/test/java/learn/features/nullpointerGuidance/UserTest.java
@@ -0,0 +1,35 @@
+package learn.features.nullpointerGuidance;
+
+import learn.features.nullPointerGuidance.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class UserTest {
+    @Test
+    void shouldThrowExceptionWithFieldName() {
+        User john = new User();
+
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> {
+            john.userDetail.address.firstLine = "ABC Apartment";
+        });
+
+        assertEquals(exception.getMessage(), "Cannot read field \"address\" because \"john.userDetail\" is null");
+
+
+        // Before Java 17
+
+            /*
+                Exception in thread "main" java.lang.NullPointerException
+                    at learn.features.nullPointerGuidance.User.main(User.java:10)
+             */
+
+        // After Java 17
+
+            /*
+            Exception in thread "main" java.lang.NullPointerException: Cannot read field "address" because "<local1>.userDetail" is null
+                at learn.features.nullPointerGuidance.User.main(User.java:10)
+             */
+    }
+}
